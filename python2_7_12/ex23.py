977b76b7a3e0c20544e97a35bd4a92c024a644c1
shadySource <alec0061@hotmail.com>
2016-08-07 19:56:36 -0400

    add 24-40

--- python2_7_12/ex23.py
+++ python2_7_12/ex23.py
@@ -1,4 +1,7 @@
-######################################
-# There won't be any code in this 
-#	exercise or the next one...
-######################################
+#######################################
+# This excercise was to examine 
+#   someone else's python program.
+# I looked at a python based keylogger.
+#######################################
+
+# https://github.com/ajinabraham/Xenotix-Python-Keylogger.git
