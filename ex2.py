a9895d579440797f12ffc0e594f2a8e9ed48251d
shadySource <alec0061@hotmail.com>
2016-08-07 14:29:24 -0400

    add python source to seperate directory for version 2.7.12

--- ex2.py
+++
@@ -1,4 +0,0 @@
-#this is a single-line comment 
-print "The stuff I commented has been ignored"
-#comments can be used to disable and quickly remove active code for testing.
-#print "This will not be printed"
